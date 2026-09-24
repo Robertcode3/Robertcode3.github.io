7e0516c5bb6604489275998560b6226f0be30c7e
Robertcode3 <Champa158@wearescpps.org>
2026-09-24 16:41:07 +0000

    adding website and portfolio

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -29,8 +29,26 @@ $(function () {
     // TODO 1 - Enable the Grid
     // toggleGrid();
 
-
-    // TODO 2 - Create Platforms
+createPlatform(0, 500, 300, 20, "blue", 0, 1000, 0, 0, 1000, 0);
+createPlatform(600, 325, 120, 20, "orange", 0, 1000, 0, 350, 425, 2);
+createPlatform(200, 200, 150, 20, "blue", 0, 200, 3, 0, 1000, 0);
+createPlatform(300, 650, 200, 20, "blue", 0, 1000, 0, 0, 1000, 0);
+createPlatform(1000, 200, 400, 20, "blue", 0, 1000, 0, 0, 1000, 0);
+createBadPlatform(1000, 420, 10, 225, "red", 0, 1000, 0, 0, 1000, 0);
+createBadPlatform(1000, 200, 10, 50, "red", 0, 1000, 0, 0, 1000, 0);
+createPlatform(1000, 600, 400, 10, "blue", 0, 10000, 0, 0, 1000, 0)
+createPlatform(1000, 600, 10, 100, "red", 0, 1000, 0, 550, 640)
+createPlatform(1100, 200, 10, 400, "blue", 0, 2000, 0, 0, 1000, 0)
+createPlatform(280, 490, 2, 10, "blue", 0, 1000, 0, 0, 1000, 0)
+  // TODO 3 - Create Collectables
+createCollectable("diamond", 200, 100, 0, 0, 0, 1000, 0)
+createCollectable("diamond", 1350, 100, 0, 0, 0, 1500, 0)
+createCollectable("diamond", 1000, 335, 0, 0, 0, 1500, 0)
+createCollectable("diamond", 1350, 700, 0, 0, 0, 1500, 0)
+  // TODO 4 - Create Cannons
+createCannon("top", 1300, 600, 10, 10, 0, 1000, 0)
+createCannon("left", 200, 1300, 10, 10, 0, 1000, 0)
+createCannon("right", 570, 1300, 10, 10, 0, 1000, 0)
 
 
 
